dfae8c9c2e00ff782388093adb739f741691b02d
dogsbean <lja0328@gmail.com>
2024-11-11 10:38:57 +0900

    Register listeners & plugin.yml added

--- src/main/java/io/dogsbean/deathanimation/Main.java
+++ src/main/java/io/dogsbean/deathanimation/Main.java
@@ -1,6 +1,8 @@
 package io.dogsbean.deathanimation;
 
+import io.dogsbean.deathanimation.listeners.PlayerListener;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -11,5 +13,11 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+
+        registerListeners();
+    }
+
+    private void registerListeners() {
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
     }
 }
